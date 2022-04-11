75342db2e34b7980e3dd0ede024b26b7fef674f0
KWAKMB <kki9621@naver.com>
2022-04-11 13:53:13 +0900

    Add files via upload

---
+++ 다변량분석/qqnorm.r
@@ -0,0 +1,20 @@
+# 다변량 정규분포
+
+mydata <- read.csv('./data/나이와수입.csv')
+n <- dim(mydata)[1]
+t <- seq(1,n)
+age_s <- sort(mydata$age); income_s <- sort(mydata$income)
+
+edf <- (t-0.5)/n  # or t/(n+1)
+q <- qnorm(edf)
+
+cbind(t, age_s, edf, q)
+cbind(t, income_s, edf, q)
+
+qqnorm(mydata$age)
+qqline(mydata$age)
+
+
+qqnorm(mydata$income)
+qqline(mydata$income)
+
