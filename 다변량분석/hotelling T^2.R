c8baf338e7d08060b9c5a0e870cc92d8ac6d2af9
KWAKMB <kki9621@naver.com>
2022-06-07 19:21:23 +0900

    Add files via upload

--- 다변량분석/hotelling T^2.R
+++ 다변량분석/hotelling T^2.R
@@ -15,9 +15,11 @@ S_inverse = solve(cov(df)) # S^-1 (역행렬)
 mu = matrix(c(4,50,10), ncol = 1)
 x_bar = matrix(colMeans(df), ncol = 1)
 n = nrow(df)
-
+p = ncol(df)
 # t^2 통계량
 T_sq = n*t(x_bar-mu)%*%S_inverse%*%(x_bar-mu)
 
-
-t.test(df)
+(n-1)*p / (n-p)
+qf(0.95,p,n-p) # 
+(n-1)*p / (n-p) * qf(0.95,p,n-p)
+# t^2이 10.73보다 크지않으므로 h0기각 X
